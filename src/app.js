8f3596801cb948af494de01a25ccacaaa955ebb4
Salmahussein430 <94374813+Salmahussein430@users.noreply.github.com>
2021-12-24 19:07:44 +0000

    used api to integrate the current temperature into the weather forecast.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "49fcd8b8be4b347d531351f264a4f0d0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+
 function showWeather(response) {
   document.querySelector("h1").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
@@ -19,6 +26,8 @@ function showWeather(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -71,20 +80,34 @@ let day = days[now.getDay()];
 
 document.querySelector("#date-time").innerHTML = `${day}, ${hour}:${minutes}`;
 
-function showForecast() {
-  let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+  return day;
+}
+
+function showForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    let minTemp = Math.round(forecastDay.temp.min);
+    let maxTemp = Math.round(forecastDay.temp.max);
+    let icon = forecastDay.weather[0].icon;
+
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
               <div class="col-2">
                 <div class="weather-forecast-preview">
-                  <div class="weather-day">${day}</div>
+                  <div class="weather-day">${formatDay(forecastDay.dt)}</div>
                   <div class="weather-icons">
                     <img
-                      src="http://openweathermap.org/img/wn/04d@2x.png"
+                      src="http://openweathermap.org/img/wn/${icon}@2x.png"
                       alt="clouds"
                       class="weather-icon-1"
                       id="icon-1"
@@ -93,12 +116,13 @@ function showForecast() {
                     />
                   </div>
                   <div class="forecast-temperature">
-                    <span class="max-temp">5°</span>
-                    <span class="min-temp">1°</span>
+                    <span class="max-temp">${maxTemp}°</span>
+                    <span class="min-temp">${minTemp}°</span>
                   </div>
                 </div>
               </div>
             `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
